e42510f048bdb7e86adaaf69a4d4c4df88ad2b29
goldenDelight <sk8pirate24@gmail.com>
2018-06-21 19:23:54 -0400

    time

--- src/controllers/FoundController.java
+++ src/controllers/FoundController.java
@@ -9,7 +9,6 @@ import models.FoundModel;
 import java.io.IOException;
 
 public class FoundController {
-
     private DicModel dictionary = DicModel.getModel();
     private FoundModel found = FoundModel.getModel();
     private static WipController wipController = new WipController();
@@ -20,6 +19,30 @@ public class FoundController {
     @FXML
     Label foundCount = new Label();
 
+//    Binds ListView of foundView and ObservableList of foundModel
+    public void initialize(){
+
+        System.out.println("Found Controller Initialized");
+
+
+        found.linkList(foundListView, foundCount);
+        found.setCount(0);
+
+        try {
+            dictionary.DicMaker();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void giveUp(){
+        found.compileFinalList(foundListView);
+    }
+
+    public void updateDic(String[][] newLetters){
+
+        for(int i = 0,  r = 0; r < 4; r++){
             for(int c = 0; c < 4; c++){
                 System.out.print(newLetters[r][c]);;
             }
--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -5,4 +5,36 @@ import javafx.event.ActionEvent;
 public class MenuController {
 
     final private static FoundController FOUND_CONTROLLER = new FoundController();
-    final pr
+    final private static BoardController BOARD_CONTROLLER = new BoardController();
+    final private static GridController GRID_CONTROLLER = new GridController();
+    final private static GraphicsController GRAPHICS_CONTROLLER = new GraphicsController();
+
+    public void submitButton(ActionEvent actionEvent, String wip) {
+        FOUND_CONTROLLER.submitWIP(wip);
+        BOARD_CONTROLLER.clearWIP();
+        GRID_CONTROLLER.resetButtons();
+        GRAPHICS_CONTROLLER.clearCircles();
+    }
+
+    public void cancelButtonClicked(ActionEvent actionEvent) {
+        BOARD_CONTROLLER.clearWIP();
+        GRID_CONTROLLER.resetButtons();
+        GRAPHICS_CONTROLLER.clearCircles();
+    }
+
+    public void rollButtonClicked(ActionEvent actionEvent) {
+        BOARD_CONTROLLER.clearWIP();
+        FOUND_CONTROLLER.newGame();
+        GRAPHICS_CONTROLLER.clearCircles();
+        GRID_CONTROLLER.newRound();
+    }
+
+    public void updateDic(String[][] newLetters){
+
+        FOUND_CONTROLLER.updateDic(newLetters);
+    }
+
+    public void close(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
