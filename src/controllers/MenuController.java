ac9d002ace890264f99f120163145da3e156e9b4
goldenDelight <sk8pirate24@gmail.com>
2018-06-21 18:55:42 -0400

    scrap test

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -5,36 +5,4 @@ import javafx.event.ActionEvent;
 public class MenuController {
 
     final private static FoundController FOUND_CONTROLLER = new FoundController();
-    final private static WipController BOARD_CONTROLLER = new WipController();
-    final private static GridController GRID_CONTROLLER = new GridController();
-    final private static GraphicsController GRAPHICS_CONTROLLER = new GraphicsController();
-
-    public void submitButton(ActionEvent actionEvent, String wip) {
-        FOUND_CONTROLLER.submitWIP(wip);
-        BOARD_CONTROLLER.clearWIP();
-        GRID_CONTROLLER.resetButtons();
-        GRAPHICS_CONTROLLER.clearCircles();
-    }
-
-    public void cancelButtonClicked(ActionEvent actionEvent) {
-        BOARD_CONTROLLER.clearWIP();
-        GRID_CONTROLLER.resetButtons();
-        GRAPHICS_CONTROLLER.clearCircles();
-    }
-
-    public void rollButtonClicked(ActionEvent actionEvent) {
-        BOARD_CONTROLLER.clearWIP();
-        FOUND_CONTROLLER.newGame();
-        GRAPHICS_CONTROLLER.clearCircles();
-        GRID_CONTROLLER.newRound();
-    }
-
-    public void updateDic(String[][] newLetters){
-
-        FOUND_CONTROLLER.updateDic(newLetters);
-    }
-
-    public void close(ActionEvent actionEvent) {
-        System.exit(0);
-    }
-}
+    final pr
